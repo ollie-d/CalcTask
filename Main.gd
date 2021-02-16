a2c966b8a441e076024c7887a31c960d53d6367c
ollie-d <ollie.damico@gmail.com>
2021-02-16 12:54:57 -0800

    Cleaned up code by removing dead code and pass statements.

--- Main.gd
+++ Main.gd
@@ -46,17 +46,12 @@ func _ready():
 	$wheel.visible = false
 	$wheel.modulate = Color(.58, .89, .85, 0.85)
 	$easterEgg.visible = false
-	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$wheel.rotation_degrees += 3
 	if $wheel.rotation_degrees == 360:
 		$wheel.rotation_degrees = 0 
-	#$problem/questionTimeProg.set_value($problem/questionTimeProg.get_value() - (100/(QUESTION_TIME/delta))) # fix
-	#if $problem/questionTimeProg.get_value() == 0:
-	#	$questionTimer.stop()
-#	pass
 
 func _on_Button_pressed():
 	$calcTimer.start(CALC_DELAY) # delay calc opening
@@ -98,11 +93,6 @@ func next_question():
 	$questionTimer.start(delta_qtime)
 
 func _on_submit_pressed():
-	#$calculator.visible = false
-	#$wheel.visible = false
-	#$calculator.stored = 0
-	#$calculator.currentOp = ""
-	#$calculator/Panel/VBoxContainer/Display.set_text(str(0))
 	if int($problem/answer.text) == questions[question][1]:
 		$scoreLabel/score.text = str(int($scoreLabel/score.text)+1)
 		$feedback.bbcode_text = '[color=#00FF00]+1[/color]'
@@ -116,36 +106,19 @@ func _on_submit_pressed():
 	$feedback.modulate.a = 1
 	$feedback.visible = true
 	$feedbackTimer.start(0.1)
-	#question += 1
-	#if question >= len(questions):
-	#	question = 0
-	#$problem.text = questions[question][0]
-	#$problem/answer.text = ''
-	
-	# Set focus back to answer box for convenience
-	#$problem/answer.grab_focus()
-	
-	# Refresh questionTimer
-	#$questionTimer.stop()
-	#$problem/questionTimeProg.set_value($problem/questionTimeProg.max_value)
-	#$problem/questionTimeProg.value = $problem/questionTimeProg.max_value
-	#$questionTimer.start(delta_qtime) 
 	next_question()
 
 func _on_feedbackTimer_timeout():
-	# Move feedback up and make transparent
-	#$feedback.rect_position = (Vector2($feedback.rect_position()[0], $feedback.rect_position()[0] + 5))
+	# Make transparent
 	$feedback.modulate.a -= 0.2;
 	if $feedback.modulate.a == 0:
 		$feedbackTimer.stop()
 		$feedback.visible = false
-	pass # Replace with function body.
 
 func _on_calculator_equals_pressed():
 	able_calculator(false)
 	$wheel.visible = true
 	$equalTimer.start(MAX_DELAY) # subtract time elapsed so far
-	pass # Replace with function body.
 
 func able_calculator(x):
 # If true, enable, if false, disable
@@ -165,42 +138,22 @@ func _on_equalTimer_timeout():
 	emit_signal("show_answer")
 	able_calculator(true)
 	$wheel.visible = false
-	pass # Replace with function body.
 
 func _on_easterEggTimer_timeout():
 	$easterEgg.modulate.a -= 0.2
 	if $easterEgg.modulate.a == 0:
 		$easterEggTimer.stop()
 		$easterEgg.visible = false
-	pass # Replace with function body.
 
 func _on_questionTimer_timeout():
 	var sub = $problem/questionTimeProg.max_value / (QUESTION_TIME/delta_qtime)
 	$problem/questionTimeProg.value = $problem/questionTimeProg.value - sub
-	#print($problem/questionTimeProg.value)
-	#- ((QUESTION_TIME*delta_qtime)) # fix
-	#$problem/questionTimeProg.set_value($problem/questionTimeProg.get_value() 
-								  
+
 	if $problem/questionTimeProg.get_value() <= 0:
-		#$questionTimer.stop()
-	
-		#$calculator.visible = false
-		#$wheel.visible = false
-		#$calculator.stored = 0
-		#$calculator.currentOp = ""
 		$scoreLabel/score.text = str(int($scoreLabel/score.text)-1)
 		$feedback.bbcode_text = '[color=#FF0000]-1[/color]'
 		$feedback.modulate.a = 1
 		$feedback.visible = true
 		$feedbackTimer.start(0.1)
-		#question += 1
-		#if question >= len(questions):
-		#	question = 0
-		#$problem.text = questions[question][0]
-		#$problem/answer.text = ''
-		#$problem/questionTimeProg.value = $problem/questionTimeProg.max_value
-		#$questionTimer.start(delta_qtime)
 		
-		# Set focus back to answer box for convenience
-		#$problem/answer.grab_focus()
 		next_question()
