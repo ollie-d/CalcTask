ca58f64d383550a8f861752135a12cdb574c9c60
ollie-d <ollie.damico@gmail.com>
2021-03-03 13:43:52 -0800

    Calc task working with level modulation. Just met with team and we want to use a distribution of difficulties instead so I will add  that. This current iteration is fully functional I believe but not tested so figured I'd push it in its current state

--- Main.gd
+++ Main.gd
@@ -4,41 +4,63 @@ signal show_answer
 # TODO:
 # - Add a skip button
 #    - Skip button can only be used in the first N seconds
-
-# Create logging variables
-
-# Declare member variables here. Examples:
-#var questions = [
-#	['1+1', 2],
-#	['2+2', 4],
-#	['3+3', 6]
-#]
-
-# Staircasing difficulty and cost of calculator
-# Have fewer classes of problems in order to avoid this non-linear ramp up
+# - Create logging variables
+# - Staircasing difficulty and cost of calculator
+# 	-Have fewer classes of problems in order to avoid this non-linear ramp up
 
 #var qs = preload("res://questions.gd")
 var questions = []
-var question = 0
+var difficulties = []
+var new_questions = []
+var new_difficulties = []
+var question = 0 # keep track of where we are in the block
 var feedbackColor = Color(0, 1, 0, 1);
 var CALC_DELAY = 0.1 # to open calc
 var MAX_DELAY = 3 # to get answer from equal
 var QUESTION_TIME = 20;
 var BLOCK_SIZE = 11;
-var block_counter = 0; # keep track of where we are in the block
+var TAR_CALC_USE_HARD = 5; # we want the user to use the calc 5 times
+var TAR_CALC_USE_EASY = 0;
 var num_easy = 5
-var e = [1, 1, 1, 1, 1]
-var h = [1, 1, 2, 2, 1]
+var num_hard = BLOCK_SIZE - 1 - num_easy
 var block_accuracy = 0
+var calc_use_easy = 0
+var calc_use_hard = 0
 var delta_qtime = 0.1
 var qtime = 0;
 var random = RandomNumberGenerator.new()
+
+# Define levels
+var level_hard = 5
+var level_easy = 1
+var LEVELS = [
+	[1, 1, 1, 1, 1], # Level 1: 4+3      #  2 2
+	[1, 1, 2, 3, 1], # Level 2: 8+7+2    #  3 3 3
+	[1, 2, 1, 1, 1], # Level 3: 4+13     #  5 5 5 5 5 <--- starting mean
+	[1, 1, 3, 4, 1], # Level 4: 6+3+8+7  #  3 3 3
+	[1, 2, 2, 2, 1], # Level 5: 7+4+46   #  2 2
+	[2, 2, 1, 0, 2], # Level 6: 23+78
+	[1, 2, 3, 3, 1], # Level 7: 3+8+6+57
+	[1, 2, 2, 1, 2], # Level 8: 7+23+78
+	[2, 3, 1, 1, 1], # Level 9: 23+103
+	[2, 2, 3, 0, 4], # Level 10: 57+23+78+46
+	[2, 3, 2, 2, 1], # Level 11: 41+28+345
+	[2, 3, 3, 3, 1], # Level 12: 41+57+28+345
+	[3, 3, 2, 0, 3], # Level 13: 145+341+932
+	[3, 3, 3, 0, 4]  # Level 14: 666+145+341+932
+]
+var e = LEVELS[level_easy-1]
+var h = LEVELS[level_hard-1]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_process_input(true)
 	random.randomize()
-	questions = generate_block(BLOCK_SIZE, num_easy, e, h)
+	var qs = generate_block(BLOCK_SIZE, num_easy, e, h)
+	questions = qs[0]
+	print(questions)
+	difficulties = qs[1]
+	print(difficulties)
 
 	$problem/questionTimeProg.value = $problem/questionTimeProg.max_value
 	#$questionTimer.start(delta_qtime)
@@ -54,7 +76,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$wheel.rotation_degrees += round(0.021/delta) # try to keep this constant
+	$wheel.rotation_degrees += round(0.021/max(delta, 0.006)) # try to keep this constant
 	if $wheel.rotation_degrees >= 360:
 		$wheel.rotation_degrees = 0 
 		
@@ -70,7 +92,7 @@ func generate_number(num_dig):
 	var len_i = len(choices_i)-1
 
 	var q = ''
-	for j in range(num_dig-1):
+	for _j in range(num_dig-1):
 		q += str(choices_i[random.randi_range(0, len_i)])
 	q += str(choices_f[random.randi_range(0, len_f)])
 	
@@ -78,10 +100,10 @@ func generate_number(num_dig):
 
 func create_problem(min_dig, max_dig, num_ops, num_min=1, num_max=1):
 	var ops = []
-	for i in range(num_min):
+	for _i in range(num_min):
 		ops.append(generate_number(min_dig))
 
-	for i in range(num_max):
+	for _i in range(num_max):
 		ops.append(generate_number(max_dig))
 
 	if (num_max + num_min - 1) > num_ops:
@@ -103,16 +125,31 @@ func create_problem(min_dig, max_dig, num_ops, num_min=1, num_max=1):
 	return [problem, answer]
 	
 func generate_block(block_size, num_easy, e, h):
-	var questions_ = []
+	var questions_ = []; var questions__ = []
+	var difficulty_ = []; var difficulty__ = []
 	for j in range(num_easy):
 		questions_.append(create_problem(e[0], e[1], e[2], e[3], e[4]))
+		difficulty_.append('e')
 	for j in range(block_size - 1 - num_easy):
 		questions_.append(create_problem(h[0], h[1], h[2], h[3], h[4]))
-	# Final question always easy
-	questions_.shuffle()
-	questions_.append(create_problem(e[0], e[1], e[2], e[3], e[4]))
+		difficulty_.append('h')
 	
-	return questions_
+	# Generate index array, shuffle
+	var ix = range(block_size-1)
+	random.randomize()
+	ix.shuffle()
+	print(ix)
+	for i in ix:
+		questions__.append(questions_[i])
+		difficulty__.append(difficulty_[i])
+	
+	# Final question always easy
+	questions__.append(create_problem(e[0], e[1], e[2], e[3], e[4]))
+	difficulty__.append('e')
+	print(questions__)
+	print(difficulty__)
+	
+	return [questions__, difficulty__]
 
 func _on_Button_pressed():
 	$calcTimer.start(CALC_DELAY) # delay calc opening
@@ -130,6 +167,10 @@ func _on_taskTimer_timeout():
 	if int($timeLeft/timer.text) == 0:
 		$taskTimer.stop()
 
+func modulate_difficulty(difficulties, tar_hard, tru_hard, tar_easy, tru_easy):
+	# Use calculator use to modulate difficulty
+	print('A')
+
 func next_question():
 	$calculator.visible = false
 	$wheel.visible = false
@@ -139,25 +180,42 @@ func next_question():
 
 	# Increment question
 	question += 1
+	if question == BLOCK_SIZE - 1:
+		print('FINAL BLOCK')
+		# Use calculator use to modulate difficulty
+		if calc_use_easy > TAR_CALC_USE_EASY:
+			print('Making easy problems harder')
+			level_easy = max(1, level_easy-1)
+		else:
+			print('Making easy problems easier')
+			level_easy = min(level_hard, level_easy+1)
+		if calc_use_hard <= TAR_CALC_USE_HARD:
+			print('Making hard problems harder')
+			level_hard = min(len(LEVELS), level_hard+1)
+		else:
+			print('Making hard problems easier')
+			level_hard = max(level_easy, level_hard-1)
+		
+		var qs = generate_block(BLOCK_SIZE, num_easy, 
+								LEVELS[level_easy], LEVELS[level_hard])
+		new_questions = qs[0]
+		new_difficulties = qs[1]
+	elif question == BLOCK_SIZE:
+		question = 0
+		questions = new_questions
+		difficulties = new_difficulties
+	else:
+		print('Question ' + str(question))
+	
 	$problem.text = questions[question][0]
 	$problem/answer.text = ''
 	$problem/answer.grab_focus()
-	if block_counter == BLOCK_SIZE:
-		# If we're just before the final question, calculate next block
-		print('A');
-	elif block_counter == BLOCK_SIZE:
-		print('A')
-	else:
-		#if question >= len(questions):
-		#	question = 0
-		$problem.text = questions[question][0]
-		$problem/answer.text = ''
-		$problem/answer.grab_focus()
 
 func _on_submit_pressed():
 	if int($problem/answer.text) == questions[question][1]:
 		$scoreLabel/score.text = str(int($scoreLabel/score.text)+1)
 		$feedback.bbcode_text = '[color=#00FF00]+1[/color]'
+		block_accuracy += 1 / (BLOCK_SIZE - 1)
 	else:
 		$scoreLabel/score.text = str(int($scoreLabel/score.text)-1)
 		$feedback.bbcode_text = '[color=#FF0000]-1[/color]'
@@ -178,6 +236,10 @@ func _on_feedbackTimer_timeout():
 		$feedback.visible = false
 
 func _on_calculator_equals_pressed():
+	if difficulties[question] == 'e':
+		calc_use_easy += 1
+	elif difficulties[question] == 'h':
+		calc_use_hard += 1
 	able_calculator(false)
 	$wheel.visible = true
 	$equalTimer.start(MAX_DELAY) # subtract time elapsed so far
